ca8cd0a709390f3534508c3fcb9689eaa669d7cd
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2014-09-26 23:22:40 +0200

    bootstraping on vultr vm
    http://www.vultr.com/?ref=6811816

---
+++ vm-centOS6-64/repo/init-vultr.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#  Init for vultr VM (or any brand new centos7)
+#
+
+# addon pour installer le repo puppet
+yum localinstall http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+
+
+# git puppet
+yum install -y git puppet
+
+# init repo (ro)
+cd /opt && git clone https://github.com/smougenot/devops_puppet_workshop.git
